0c88163612a91927991bc492a5ffd0c86ced022d
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-22 22:03:21 -0300

    Separando as Rotas

--- src/server.js
+++ src/server.js
@@ -1,20 +1,11 @@
 import http from "node:http"
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
+
 
 const server = http.createServer(async (request, response) => {
-  const { method, url } = request // Desestruturação
-
   await jsonBodyHandler(request, response)
-
-  if (method === "GET" && url === "/products") {
-    return response.end("Lista de produtos!")
-  }
-
-  if (method === "POST" && url === "/products") {
-    return response.writeHead(201).end(JSON.stringify(request.body))
-  }
-
-  return response.writeHead(404).end("Rota não encontrada!")
+  routeHandler(request, response) // Não precisa do await, pois não é assincrona
 })
 
 server.listen(3333)
